63be2d19c329e40af8ab1ea3c6d927cd9c8d9ae6
Jonas Hillenbrand <32593690+jhillenbrand@users.noreply.github.com>
2022-01-30 12:19:35 +0100

    Update UnitTest_Plotlify.java

--- test/plotlify/UnitTest_Plotlify.java
+++ test/plotlify/UnitTest_Plotlify.java
@@ -12,7 +12,7 @@ public class UnitTest_Plotlify {
 		double[] x = {1.0, 1.4, 1.5, 1.9, 2.0};
 		double[] y = {12.234, 43.122, 34.1234, 18.9122, 22.923};
 		
-		String filePath = "C:\\Users\\hillenbrand\\Downloads\\test\\test.html";
+		String filePath = "C:\\Users\\****\\Downloads\\test\\test.html";
 		String title = "Test1";
 		
 		Plotlify.line(filePath, x, y, title);
@@ -26,7 +26,7 @@ public class UnitTest_Plotlify {
 		double[] y = {12.234, 43.122, 34.1234, 18.9122, 22.923};
 		double[] z = {2.234, 4.122, 3.1234, 1.9122, 2.923};
 		
-		String filePath = "C:\\Users\\hillenbrand\\Downloads\\test\\test.html";
+		String filePath = "C:\\Users\\****\\Downloads\\test\\test.html";
 		String title = "Test1";
 		
 		Plotlify.scatter3D(filePath, x, y, z, title);
