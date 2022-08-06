0c478bc84579826f5a1f748d29e0dc9364ecd6bf
Jonas Hillenbrand <hillenbrandjonas@hotmail.com>
2022-08-07 00:56:27 +0200

    corrected surface template

--- src/net/sytes/botg/plotlify/templates/SurfaceTemplate.java
+++ src/net/sytes/botg/plotlify/templates/SurfaceTemplate.java
@@ -36,38 +36,13 @@ public class SurfaceTemplate extends XYZTemplate implements ITemplateSurface {
 	@Override
 	public void setData(double[] x, double[] y, double[][] z, String name) {
 		
-		String s = this.surfaceMatrix(x, y, z);
-		
-		this.inject(ZDATA_ID, s);
+		this.inject(XDATA_ID, Arrays.toString(x));
+		this.inject(YDATA_ID, Arrays.toString(y));
+		this.inject(ZDATA_ID, Arrays.deepToString(z));
 		
 		this.inject(TRACE_NAME_ID, name);		
 	}
 	
-	private String surfaceMatrix(double[] x, double[] y, double[][] z){
-		// check dimensions
-		if (z.length != y.length) {
-			throw new IllegalArgumentException("1st dimension of z must be equal to length of y");
-		}
-		if (z[0].length != x.length) {
-			throw new IllegalArgumentException("2nd dimension of z must be equal to length of x");
-		}
-		
-		double[][] z_s = new double[y.length + 1][x.length + 1];
-		
-		// populate surface matrix 
-		System.arraycopy(x, 0, z_s[0], 1, x.length);
-		for (int i = 0; i < z.length; i++) {
-			System.arraycopy(z[i], 0, z_s[i + 1], 1, x.length);
-			z_s[i + 1][0] = y[i];
-		}
-		
-		String s = Arrays.deepToString(z_s);
-		
-		// replace first entry with empty string
-		s = s.replace("[[0.0", "[[");
-		return s;
-	}
-
 	@Override
 	public void addData(double[] x, double[] y, double[][] z, String name) {
 		if (x != null && y != null && z != null) {
@@ -78,7 +53,9 @@ public class SurfaceTemplate extends XYZTemplate implements ITemplateSurface {
 			// create the text for new trace
 			StringBuilder sb1 = new StringBuilder();
 			sb1.append("\t\t\tvar trace").append(this.numberOfTraces).append(" = {\n")
-				.append("\t\t\t\tz: ").append(this.surfaceMatrix(x, y, z)).append(",\n")
+				.append("\t\t\t\tx: ").append(Arrays.toString(x)).append(",\n")
+				.append("\t\t\t\ty: ").append(Arrays.toString(y)).append(",\n")
+				.append("\t\t\t\tz: ").append(Arrays.deepToString(z)).append(",\n")
 				.append("\t\t\t\ttype: '").append(PLOT_TYPE_ID).append("',\n")
 				.append("\t\t\t\tname: '").append(name).append("'\n")
 				.append("\t\t\t};\n")
