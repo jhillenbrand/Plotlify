f4517878bd9eb312cdb8dc41e826ad2982046d6f
Jonas Hillenbrand <hillenbrandjonas@hotmail.com>
2022-11-02 07:55:52 +0100

    update

--- test/plotlify/UnitTest_PlotlifyUtils.java
+++ test/plotlify/UnitTest_PlotlifyUtils.java
@@ -1,5 +1,7 @@
 package plotlify;
 
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
 
 import net.sytes.botg.plotlify.PlotlifyUtils;
@@ -10,6 +12,16 @@ public class UnitTest_PlotlifyUtils {
 	public void test00() {
 		
 		PlotlifyUtils.downloadPlotlyLibrary("C:\\Users\\****\\Downloads\\test");
+		
+	}
+	
+	@Test
+	public void test010() {
+		
+		double[] x = {1.0, 2.0, 3.0, Double.NaN, 4.0, 5.0};		
+		double[] y = {1.0, 2.0, 3.0, Double.NaN, 4.0, 5.0};
+		
+		System.out.println(Arrays.toString(x));
 		
 	}
 	
